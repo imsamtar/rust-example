264e762ba367c1c9cd3e36913b60a79ad4c169a6
Sameer Tariq <imsamtar@gmail.com>
2021-11-27 23:14:11 +0500

    print arrays

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let chars = ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'];
+    print_array(chars)
+}
+
+fn print_array(array: [char; 11]) {
+    for i in array {
+        print!("{}", i);
+    }
+    println!("");
 }
